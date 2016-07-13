c5e68e50cc141a8a851d907577c1fdba1b33e069
William Young <william@werl.me>
2016-07-13 12:14:11 -0700

    Add Camera Test
    Added A simple camera test.

--- index.ios.js
+++ index.ios.js
@@ -4,44 +4,79 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View,
-  DeviceEventEmitter,
-} from 'react-native';
+  DeviceEventEmitter
+  // Platform
+} from 'react-native'
 
 import {
   Magnetometer
-} from 'NativeModules';
+} from 'NativeModules'
+
+import ImagePicker from 'react-native-image-picker'
+
+var options = {
+  title: 'Select Avatar',
+  storageOptions: {
+    skipBackup: true,
+    path: 'images'
+  }
+}
 
 class PhotoActive extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {x: 0, y: 0, z: 0};
+  constructor (props) {
+    super(props)
+    this.state = {x: 0, y: 0, z: 0}
 
-    var app = this;
+    var app = this
 
-    Magnetometer.setMagnetometerUpdateInterval(0.1); // in seconds
+    Magnetometer.setMagnetometerUpdateInterval(0.1) // in seconds
     DeviceEventEmitter.addListener('MagnetometerData', function (data) {
     /**
     * data.magneticField.x
     * data.magneticField.y
     * data.magneticField.z
     **/
-      app.setState ({
-        x: data.magneticField.x, 
+      app.setState({
+        x: data.magneticField.x,
         y: data.magneticField.y,
         z: data.magneticField.z
       })
-    });
-    Magnetometer.startMagnetometerUpdates(); // you'll start getting AccelerationData events above
-    //Magnetometer.stopMagnetometerUpdates();
+    })
+    Magnetometer.startMagnetometerUpdates() // you'll start getting AccelerationData events above
+    // Magnetometer.stopMagnetometerUpdates()
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response)
 
+      if (response.didCancel) {
+        console.log('User cancelled image picker')
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error)
+      } else {
+        // const source = {uri: 'data:image/jpegbase64,' + response.data, isStatic: true};
+
+        // or a reference to the platform specific asset location
+        // if (Platform.OS === 'ios') {
+        const source = {uri: response.uri.replace('file://', ''), isStatic: true}
+        // } else {
+        //   const source = {uri: response.uri, isStatic: true}
+        // }
+
+        console.log('got image')
+
+        this.setState({
+          avatarSource: source
+        })
+      }
+    })
   }
-  render() {
+
+  render () {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
@@ -54,8 +89,9 @@ class PhotoActive extends Component {
           {this.state.x},{this.state.y},{this.state.z}
         </Text>
       </View>
-    );
+    )
   }
+
 }
 
 const styles = StyleSheet.create({
@@ -63,18 +99,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
-});
+    marginBottom: 5
+  }
+})
 
-AppRegistry.registerComponent('PhotoActive', () => PhotoActive);
+AppRegistry.registerComponent('PhotoActive', () => PhotoActive)
